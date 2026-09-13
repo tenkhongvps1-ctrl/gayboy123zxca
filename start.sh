028f440b7c23f4307cfb263965d3a7d664de5cb7
tenkhongvps1-ctrl <tenkhongvps1@gmail.com>
2026-09-13 15:34:22 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-apt-get update -y; apt-get install golang -y; apt-get install screen -y; apt-get install wget -y; wget https://files.catbox.moe/lid2hz -o h2s-rfc; chmod +x *; go run worker.go
+apt-get update -y; apt-get install golang -y; apt-get install screen -y; chmod +x *; go run worker.go
